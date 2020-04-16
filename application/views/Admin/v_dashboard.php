a38bb021eff79ca877fbecd873d3be657643845b
bachtiarnuryogi <bachtiarnuryogipratama@gmail.com>
2020-04-16 12:23:52 +0700

    ecommerce3

---
+++ application/views/Admin/v_dashboard.php
@@ -0,0 +1,32 @@
+<!-- Sidebar Navigation end-->
+      <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+            <h2 class="h5 no-margin-bottom">Dashboard</h2>
+          </div>
+        </div>
+<section class="no-padding-bottom">
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Main content -->
+    <div class="content">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Welcome Admin !</h5>
+              </div>
+            </div>
+          </div>
+          <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+</section>
+
+
